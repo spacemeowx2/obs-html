058f0bdef926e96ea5ec270192bb4006c9fe9973
imspace <imspace@vip.qq.com>
2017-11-29 20:13:12 +0800

    make a class

--- js/bilibili-danmaku.js
+++ js/bilibili-danmaku.js
@@ -1,58 +1,80 @@
-function onDanmu (danmu) {
-    console.log('onDanmu', danmu)
-    addLine(`${danmu.lb}: ${danmu.text}`)
-    if (tts.length < 3) {
-        tts.addQueue(`${danmu.lb}说 ${danmu.text}`)
-    }
-}
-function onGift (giftName, count, sender) {
-    let text = `感谢${sender}的${count}个${giftName}`
-    console.log('onGift', text)
-    addLine(text)
-    tts.addQueue(text) // 不管怎样还是要谢啊(逃 辣条刷屏就算了
-}
-function onMessage (payload) {
-    try {
-        let data = JSON.parse(payload)
-        switch (data.cmd) {
-            case 'DANMU_MSG':
-                let p = data.info
-                let danmu = {
-                    ha: -1,
-                    mode: p[0][1],
-                    size: p[0][2],
-                    color: p[0][3],
-                    uid: p[2][0],
-                    wg: p[0][5],
-                    text: p[1],
-                    lb: p[2][1],
-                    I: {
-                        level: p[4][0],
-                        Sr: p[2][5],
-                        verify: !!p[2][6]
-                    }
+// @ts-check
+class BilibiliDanmakuHelper {
+    constructor (roomid) {
+        if (roomid.length > 0) {
+            this.tts = new TTS(Param.get('ttsint', 200), Param.get('volume', 0))
+            let danmu = new BilibiliDanmaku(roomid)
+            let welcome = `Bilibili 弹幕助手 启动! 当前房间号: ${roomid}`
+            this.addLine(welcome)
+            this.tts.addQueue(welcome)
+            danmu.onMessage = (pkg) => {
+                switch (pkg.op) {
+                    case 5:
+                        this.onMessage(pkg.payload)
+                        break
                 }
-                onDanmu(danmu)
-                break
-            case 'SEND_GIFT':
-                let gift = data.data
-                onGift(gift.giftName, gift.num, gift.uname)
-                break
-            default:
-                console.log('ignore unknown cmd: ', data.cmd)
+            }
+        } else {
+            this.addLine('Bilibili 弹幕助手 启动1')
+            this.tts.addQueue('请制定房间号')
         }
-    } catch (e) {
-        console.error(e)
     }
-}
-function addLine (text) {
-    const list = document.getElementById('list')
-    const danmaku = document.getElementById('danmaku')
-    const li = document.createElement('li')
-    li.innerText = text
-    list.appendChild(li)
-    if (list.children.length > 50) {
-        list.children[0].remove()
+    onDanmu (danmu) {
+        console.log('onDanmu', danmu)
+        this.addLine(`${danmu.lb}: ${danmu.text}`)
+        if (this.tts.length < 3) {
+            this.tts.addQueue(`${danmu.lb}说 ${danmu.text}`)
+        }
+    }
+    onGift (giftName, count, sender) {
+        let text = `感谢${sender}的${count}个${giftName}`
+        console.log('onGift', text)
+        this.addLine(text)
+        this.tts.addQueue(text) // 不管怎样还是要谢啊(逃 辣条刷屏就算了
+    }
+    onMessage (payload) {
+        try {
+            let data = JSON.parse(payload)
+            switch (data.cmd) {
+                case 'DANMU_MSG':
+                    let p = data.info
+                    let danmu = {
+                        ha: -1,
+                        mode: p[0][1],
+                        size: p[0][2],
+                        color: p[0][3],
+                        uid: p[2][0],
+                        wg: p[0][5],
+                        text: p[1],
+                        lb: p[2][1],
+                        I: {
+                            level: p[4][0],
+                            Sr: p[2][5],
+                            verify: !!p[2][6]
+                        }
+                    }
+                    this.onDanmu(danmu)
+                    break
+                case 'SEND_GIFT':
+                    let gift = data.data
+                    this.onGift(gift.giftName, gift.num, gift.uname)
+                    break
+                default:
+                    console.log('ignore unknown cmd: ', data.cmd)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    addLine (text) {
+        const list = document.getElementById('list')
+        const danmaku = document.getElementById('danmaku')
+        const li = document.createElement('li')
+        li.innerText = text
+        list.appendChild(li)
+        if (list.children.length > 50) {
+            list.children[0].remove()
+        }
     }
 }
 function delay (time) {
@@ -88,31 +110,7 @@ class TTS {
         })
     }
 }
-let tts = new TTS(Param.get('ttsint', 200), Param.get('volume', 0))
-function main (roomid) {
-    let danmu = new BilibiliDanmaku(roomid)
-    let welcome = `Bilibili 弹幕助手 启动! 当前房间号: ${roomid}`
-    addLine(welcome)
-    tts.addQueue(welcome)
-    danmu.onMessage = (pkg) => {
-        switch (pkg.op) {
-            case 5:
-                onMessage(pkg.payload)
-                break
-        }
-    }
-}
 /** @type {string} */
 let roomid = Param.get('roomid')
-if (roomid.length > 0) {
-    main(roomid)
-} else {
-    addLine('Bilibili 弹幕助手 启动1')
-    tts.addQueue('请制定房间号')
-}
-async function test () {
-    for (let i = 0; i < 50; i++) {
-        addLine('啊' + i.toString())
-        await delay(1000)
-    }
-}
+let helper = new BilibiliDanmakuHelper(roomid)
+window.helper = helper
