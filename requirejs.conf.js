2e567f3e7d32e7dbed828e6545304aa7fd3f9801
imspace <imspace@vip.qq.com>
2017-12-28 19:26:04 +0800

    set baseUrl

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,9 +1,10 @@
 var require = {
+    baseUrl: '.',
     paths: {
-        'axios': '../lib/axios',
-        'vue': '../lib/vue-helper',
-        'aes-js': '../lib/aes-js',
-        'bignumber.js/bignumber': '../lib/bignumber',
-        'vue-class-component': '../lib/vue-class-component',
+        'axios': 'lib/axios',
+        'vue': 'lib/vue-helper',
+        'aes-js': 'lib/aes-js',
+        'bignumber.js/bignumber': 'lib/bignumber',
+        'vue-class-component': 'lib/vue-class-component',
     }
 }
