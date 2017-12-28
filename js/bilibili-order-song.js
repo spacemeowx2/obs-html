18b5f9e261397db7eb7ba5460946b96585a70ef6
imspace <imspace@vip.qq.com>
2017-12-28 22:36:20 +0800

    fix wrong path

--- js/bilibili-order-song.js
+++ js/bilibili-order-song.js
@@ -12,7 +12,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-define(["require", "exports", "common/bilibili-danmaku", "common/param", "common/netease-music", "common/music-interface", "./view/order-song", "./common/utils"], function (require, exports, bilibili_danmaku_1, param_1, netease_music_1, music_interface_1, order_song_1, utils_1) {
+define(["require", "exports", "./common/bilibili-danmaku", "./common/param", "./common/netease-music", "./common/music-interface", "./view/order-song", "./common/utils"], function (require, exports, bilibili_danmaku_1, param_1, netease_music_1, music_interface_1, order_song_1, utils_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     class Command {
